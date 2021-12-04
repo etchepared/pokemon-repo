ed90e1d16eba2d47fb0612af0a9df76c7a253a4e
etchepared <88214684+etchepared@users.noreply.github.com>
2021-12-04 16:37:21 -0300

    database connected

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,46 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "pokemon",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      hp: {
+        type: DataTypes.STRING,
+      },
+      strength: {
+        type: DataTypes.STRING,
+      },
+      defense: {
+        type: DataTypes.STRING,
+      },
+      speed: {
+        type: DataTypes.STRING,
+      },
+      height: {
+        type: DataTypes.STRING,
+      },
+      weight: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
